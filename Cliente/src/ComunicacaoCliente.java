4bc67a8488a5450ebb4218e786f113ab019f2d8f
ruimar <ruimar17@gmail.com>
2015-10-05 18:31:03 +0100

    nigga
    esse

--- Cliente/src/ComunicacaoCliente.java
+++ Cliente/src/ComunicacaoCliente.java
@@ -1,6 +1,9 @@
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.Scanner;
 
 
@@ -25,7 +28,44 @@ public class ComunicacaoCliente implements Runnable{
 	@Override
 	public void run() {
 		
+		try {
+			ligacao("hello");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
+	}
+	
+	private String ligacao(String pedido) throws UnknownHostException, IOException{
+		
+		socket = new Socket(ip, port);
+		String resposta = null;
+		
+		try {
+			out = new PrintWriter(socket.getOutputStream(), true);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		try {
+			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		out.println(pedido);
+		
+		try {
+            resposta = in.readLine();
+
+        } catch (IOException e) {
+        	resposta = ligacao(pedido);
+        }
+		
+		return resposta;
 		
 	}
 	
---
+++ ServidorRMI/src/ServidorRMI/RMIServer.java
@@ -0,0 +1,56 @@
+package ServidorRMI;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.rmi.AccessException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
+
+import rmiinterface.RMIInterface;
+
+public class RMIServer extends UnicastRemoteObject implements RMIInterface {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+
+	protected RMIServer() throws RemoteException {
+		super();
+	}
+	
+	
+	public static void main(String [] args) throws IOException{
+		
+		RMIInterface ci = new RMIServer();
+		System.setProperty("java.rmi.server.hostname", "127.0.0.1");
+		
+		try {
+			LocateRegistry.createRegistry(25055).rebind("calc", ci);
+		} catch (AccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        System.out.println("Servidor RMI ligado!");
+		
+	}
+
+
+	@Override
+	public String helloWorld() {
+		System.out.println("Hello World!");
+		return "lel";
+	}
+	
+	//funcionalidades do RMI
+
+}
